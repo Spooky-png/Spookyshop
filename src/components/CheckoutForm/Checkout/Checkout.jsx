399fe23adeee8d388784fa35b7d7c8e81bc5c4f3
Spooky-png <MikeHinds4@gmail.com>
2021-02-12 12:35:39 -0500

    Bug with selection domestic shipping option

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -4,12 +4,14 @@ import { commerce } from '../../../lib/commerce';
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
+import { SentimentVerySatisfied } from '@material-ui/icons';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = ({ cart }) => {
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
+    const [shippingData, setShippingData] = useState({});
     const classes = useStyles();
 
     useEffect(() => {
@@ -26,6 +28,16 @@ const Checkout = ({ cart }) => {
         generateToken();
     }, [cart]);
 
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+
+    const next = (data) => {
+        setShippingData(data);
+
+        nextStep();
+    }
+
     const Confirmation = () => (
         <div>
             Confirmation
@@ -33,8 +45,8 @@ const Checkout = ({ cart }) => {
     );
 
     const Form = () => activeStep === 0
-        ? <AddressForm checkoutToken={checkoutToken}/>
-        : <PaymentForm />
+        ? <AddressForm checkoutToken={checkoutToken} next={next} />
+        : <PaymentForm shippingData={shippingData}/>
 
     return (
         <>
