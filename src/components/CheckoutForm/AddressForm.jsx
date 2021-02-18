f3e53d56f73cd265f180fbc73b66a15e676fa7fa
Spooky-png <MikeHinds4@gmail.com>
2021-02-17 21:23:10 -0500

    I don't get it

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -35,7 +35,7 @@ const AddressForm = ({ checkoutToken, next }) => {
     
         setShippingOptions(options);
         setShippingOption(options[0].id);
-    {/*this bug will be the death of me */}
+    {/*this bug IS THE death of me */}
       };
 
 
